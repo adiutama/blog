ff9d3114b93ec044ba14c9b41663b611f4edb62d
adiutama <neogatotkaca@gmail.com>
2019-03-05 01:09:09 +0700

    Update template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Adi Utama`,
+    description: `Personal blog`,
+    author: `@adiutama`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -18,17 +19,23 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `adiutama`,
+        short_name: `adiutama`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#c62641`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Domine:400,700`,
+          `Josefin Sans:400,600,700`
+        ]
+      }
+    }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,123 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import classNames from 'classnames'
+import moment from 'moment'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { Helmet } from 'react-helmet'
+
+import style from '@/styles/page.module.scss'
+
+const article = [
+  {
+    title: `A beautiful blog with no images required`,
+    datetime: `2018-07-24`,
+    author: `Adi Utama`,
+    eta: `1 Minute Read`,
+    content: `Typography is a WordPress theme created for bloggers that just want to write, without the hassle of looking for the right featured image. It has a unique design based on beautiful typography and a modern, clean layout. Simply write your content and publish will handle the rest. Efficiently negotiate enabled partnerships whereas team building channels. Competently visualize cross-platform...`,
+    url: `a-beautiful-blog-with-no-images-required`,
+  },
+  {
+    title: `Now leave before I am forced to taunt you`,
+    datetime: `2018-07-01`,
+    author: `Adi Utama`,
+    eta: `1 Minute Read`,
+    content: `Credibly customize visionary niches before alternative services. Compellingly restore effective total linkage with sticky resources. Appropriately reconceptualize multifunctional leadership skills without turnkey outsourcing. Efficiently negotiate enabled partnerships whereas team building channels. Dynamically disseminate customer directed quality vectors whereas go forward potentialities. Seamlessly actualize goal-oriented methodologies whereas value-added innovation. Conveniently impact B2B manufactured products without magnetic core competencies. Synergistically...`,
+    url: `now-leave-before-i-am-forced-to-taunt-you`,
+  },
+  {
+    title: `What could possibly go wrong?`,
+    datetime: `2018-06-08`,
+    author: `Adi Utama`,
+    eta: `1 Minute Read`,
+    content: `Dynamically underwhelm integrated outsourcing via timely models. Rapidiously reconceptualize visionary imperatives without 24/365 catalysts for change. Completely streamline functionalized models and out-of-the-box functionalities. Authoritatively target proactive vortals. Globally iterate parallel content vis-a-vis just in time expertise. Collaboratively cultivate viral methods of empowerment without interactive manufactured products. Proactively exploit process-centric supply chains for distributed imperatives. Authoritatively empower error-free total...`,
+    url: `what-could-possibly-go-wrong?`,
+  },
+  {
+    title: `The simplest ways to choose the best coffee`,
+    datetime: `2018-03-04`,
+    author: `Adi Utama`,
+    eta: `1 Minute Read`,
+    content: ` Professionally mesh inexpensive experiences without empowered imperatives. Conveniently synergize turnkey e-commerce and compelling information. Credibly conceptualize installed base internal or “organic” sources through scalable mindshare. Uniquely aggregate efficient total. Objectively brand magnetic ideas with error-free interfaces. Completely orchestrate seamless manufactured products via turnkey solutions. Holisticly streamline premium strategic theme areas vis-a-vis high standards in platforms. Assertively supply mission-critical...`,
+    url: `the-simplest-ways-to-choose-the-best-coffee`,
+  },
+]
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <React.Fragment>
+    <Helmet>
+      <title>Adi Utama | Home</title>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+    </Helmet>
+
+    <header className={style.header}>
+      <div className={style.headerWrapper}>
+        <h1 className={style.headerTitle}>Typography</h1>
+
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </nav>
+      </div>
+    </header>
+
+    <main className={style.content}>
+      <div className={style.contentWrapper}>
+        <h1 className={style.contentTitle}>Latest Posts</h1>
+
+        {article.map((item, key) => (
+          <section className={style.postSummary} key={key}>
+            <time dateTime={item.datetime} className={style.postDate}>
+              <span className={style.postDateItem}>{moment(item.datetime).format(`DD`)}</span>
+              <span className={style.postDateItem}>{moment(item.datetime).format(`MMM YYYY`)}</span>
+            </time>
+
+            <article className={style.post}>
+              <h1 className={style.postTitle}>
+                <a href={item.url}>{item.title}</a>
+              </h1>
+
+              <aside className={style.postMeta}>
+                <span className={style.postMetaItem}>By {item.author}</span>
+                <span className={style.postMetaItem}>{item.eta}</span>
+              </aside>
+
+              <p>{item.content}</p>
+
+              <a href={item.url} className={classNames(style.btn, style.btnPrimary)}>Read More</a>
+            </article>
+          </section>
+        ))}
+
+        <footer className={style.pagination}>
+          <a href="/">&laquo; Newer</a>
+          <a href="/?page=1">Older &raquo;</a>
+        </footer>
+      </div>
+    </main>
+
+    <footer className={style.footer}>
+      <div className={style.footerWrapper}>
+        <div className={style.footerLinks}>
+          <a href="https://instagram.com/adiutama">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://linkedin.com/in/adiutamaa">
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a href="https://github.com/adiutama">
+            <i className="fab fa-github"></i>
+          </a>
+          <a href="https://angel.co/adiutama">
+            <i className="fab fa-angellist"></i>
+          </a>
+        </div>
+
+        <div className={style.copyright}>
+          Design By <a href="http://www.themexpose.com/">ThemeExpose</a><br />
+          All rights reserved
+        </div>
+      </div>
+    </footer>
+  </React.Fragment>
 )
 
 export default IndexPage
